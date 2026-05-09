a134b34d3c01a7636213b733ff0c89877420b141
HarryKerans <harry.kerans2@nhs.net>
2026-05-09 17:11:44 +0100

    typescript refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
+import type { ReactElement } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(): ReactElement {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edited <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
